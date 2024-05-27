448cda495157ef60ddff58ae44ea7428ae3051f4
Ivan Kevra <i.kevra@horizont-rnd.by>
2024-05-27 12:22:50 +0300

    updated styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,29 @@
-import logo from "@/assets/icons/logo.svg"
-import styles from "./header.module.scss"
-import { Typography } from "../typography"
-import notifications from "@/assets/icons/notifications.svg"
-import userPhoto from "@/assets/icons/userPhoto.svg"
-import arrowDown from "@/assets/icons/arrowDown.svg"
+import arrowDown from '@/assets/icons/arrowDown.svg'
+import logo from '@/assets/icons/logo.svg'
+import notifications from '@/assets/icons/notifications.svg'
+import userPhoto from '@/assets/icons/userPhoto.svg'
+
+import styles from './header.module.scss'
+
+import { Typography } from '../typography'
 export const Header = () => {
-    return <div className={styles.container}>
-        <img src={logo} />
-        <div className={styles.nav}>
-            <Typography variant="body1">Меню</Typography>
-            <Typography variant="body1">Вопросы и ответы</Typography>
-            <Typography variant="body1">Об АИС</Typography>
+  return (
+    <div className={styles.container}>
+      <img src={logo} />
+      <div className={styles.nav}>
+        <Typography variant={'body1'}>Меню</Typography>
+        <Typography variant={'body1'}>Вопросы и ответы</Typography>
+        <Typography variant={'body1'}>Об АИС</Typography>
+      </div>
+      <div className={styles.profileAndNotifications}>
+        <img src={notifications} />
+        <div className={styles.line}></div>
+        <div className={styles.profile}>
+          <img src={userPhoto} />
+          <Typography variant={'body1'}>Вход в аккаунт</Typography>
+          <img src={arrowDown} />
         </div>
-        <div className={styles.profileAndNotifications}>
-            <img src={notifications} />
-            <div className={styles.line}></div>
-            <div className={styles.profile}>
-                <img src={userPhoto} />
-                <Typography variant="body1">Вход в аккаунт</Typography>
-                <img src={arrowDown} />
-            </div>
-        </div>
+      </div>
     </div>
+  )
 }
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,74 +1,87 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, Navigate } from 'react-router-dom'
 
-import { Header } from "@/components/header"
-import styles from "./login.module.scss"
-import { Footer } from "@/components/footer"
-import { Typography } from "@/components/typography"
-import { TextField } from "@/components/textField"
-import { Button } from "@/components/button"
-import { useDispatch, useSelector } from 'react-redux';
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
-import { authActions } from "./login.slice"
-import { AppRootStateType } from "@/app/store"
-import { Link, Navigate } from "react-router-dom"
-import { useEffect } from "react"
-import { ArrowLeft } from "@/assets/icons/arrowLeft"
+import { AppRootStateType } from '@/app/store'
+import { ArrowLeft } from '@/assets/icons/arrowLeft'
+import { Button } from '@/components/button'
+import { Footer } from '@/components/footer'
+import { Header } from '@/components/header'
+import { TextField } from '@/components/textField'
+import { Typography } from '@/components/typography'
+import { GoogleLogin } from '@react-oauth/google'
+import { jwtDecode } from 'jwt-decode'
+
+import styles from './login.module.scss'
+
+import { authActions } from './login.slice'
 
 export const Login = () => {
+  const dispatch = useDispatch()
+  const isAuthenticated = useSelector((state: AppRootStateType) => state.auth.isAuthenticated)
 
-    const dispatch = useDispatch();
-    const isAuthenticated = useSelector((state: AppRootStateType) => state.auth.isAuthenticated)
+  useEffect(() => {
+    const token = sessionStorage.getItem('token')
 
-    useEffect(() => {
-        const token = sessionStorage.getItem("token")
-        if (token) {
-            const decoded: any = jwtDecode(token);
-            const expirationDate = new Date(decoded.exp * 1000);
-            const currentDate = new Date();
-            if (currentDate > expirationDate) {
-                dispatch(authActions.isAuthenticated(false))
-            } else {
-                dispatch(authActions.isAuthenticated(true))
-            }
-        }
-    }, [])
+    if (token) {
+      const decoded: any = jwtDecode(token)
+      const expirationDate = new Date(decoded.exp * 1000)
+      const currentDate = new Date()
 
-    const getToken = (credentialResponse: any) => {
-        const decoded: any = jwtDecode(credentialResponse.credential || '');
-        dispatch(authActions.login({
-            name: decoded.given_name,
-            familyName: decoded.family_name
-        }))
-        sessionStorage.setItem("token", credentialResponse.credential);
+      if (currentDate > expirationDate) {
+        dispatch(authActions.isAuthenticated(false))
+      } else {
+        dispatch(authActions.isAuthenticated(true))
+      }
     }
+  }, [])
 
-    if (isAuthenticated) {
-        return <Navigate to="/" />
-    }
-    return <div className={styles.container}>
-        <Header />
+  const getToken = (credentialResponse: any) => {
+    const decoded: any = jwtDecode(credentialResponse.credential || '')
 
-        <div className={styles.content}>
-            <Link to={'/'} className={styles.link}><ArrowLeft />Вернуться на главную</Link>
-            <Typography variant="h1" as="h1">Вход</Typography>
-            <div className={styles.inputs}>
-                <TextField label="Логин" placeholder="Введите логин" />
-                <TextField label="Пароль" placeholder="Введите пароль" variant="password" />
-                <Button className={styles.button}>Вход</Button>
+    dispatch(
+      authActions.login({
+        familyName: decoded.family_name,
+        name: decoded.given_name,
+      })
+    )
+    sessionStorage.setItem('token', credentialResponse.credential)
+  }
 
-                <Button variant="white" className={styles.button}>Авторизация с использованием ЕС ИФЮЛ</Button>
-                <div className={styles.googleLogin}>
-                    <GoogleLogin
-                        onSuccess={getToken}
-                        onError={() => {
-                            alert('Login Failed');
-                        }}
-                    />
-                </div>
+  if (isAuthenticated) {
+    return <Navigate to={'/'} />
+  }
 
-            </div>
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.content}>
+        <Link className={styles.link} to={'/'}>
+          <ArrowLeft />
+          Вернуться на главную
+        </Link>
+        <Typography as={'h1'} variant={'h1'}>
+          Вход
+        </Typography>
+        <div className={styles.inputs}>
+          <TextField label={'Логин'} placeholder={'Введите логин'} />
+          <TextField label={'Пароль'} placeholder={'Введите пароль'} variant={'password'} />
+          <Button className={styles.button}>Вход</Button>
+
+          <Button className={styles.button} variant={'white'}>
+            Авторизация с использованием ЕС ИФЮЛ
+          </Button>
+          <div className={styles.googleLogin}>
+            <GoogleLogin
+              onError={() => {
+                alert('Login Failed')
+              }}
+              onSuccess={getToken}
+            />
+          </div>
         </div>
-        <Footer />
+      </div>
+      <Footer />
     </div>
+  )
 }
-
